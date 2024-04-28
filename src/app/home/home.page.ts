42b9618b68693576e5c8e2803776e3e12e332c64
8aChristian <145133332+8aChristian@users.noreply.github.com>
2024-04-27 20:05:46 -0500

    SumativaFinal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { LocalNotifications } from '@capacitor/local-notifications';
-import { Database, ref, get } from '@angular/fire/database';
+import { Database, ref, onValue, Unsubscribe } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
+  luzStatus: boolean = true; // Inicializar el estado de la luz como verdadero por defecto
+  luzSubscription: Unsubscribe | null = null; // Inicializar luzSubscription como null
 
   constructor(private database: Database) {}
 
@@ -15,19 +17,28 @@ export class HomePage implements OnInit {
     // Obtener una referencia al nodo 'Luz' en tu base de datos
     const luzRef = ref(this.database, 'Luz');
 
-    // Obtener los datos una vez (sin escuchar cambios)
-    get(luzRef).then((snapshot) => {
-      const luzStatus = snapshot.val();
-      if (luzStatus) {
-        // Si Luz es true (día), muestra la notificación de día
-        this.mostrarNotificacionDia();
-      } else {
-        // Si Luz es false (noche), muestra la notificación de noche
-        this.mostrarNotificacionNoche();
-      }
-    }).catch((error) => {
+    try {
+      // Escuchar cambios en el estado de la luz
+      this.luzSubscription = onValue(luzRef, (snapshot) => {
+        this.luzStatus = snapshot.val();
+        if (this.luzStatus) {
+          // Si Luz es true (día), muestra la notificación de día
+          this.mostrarNotificacionDia();
+        } else {
+          // Si Luz es false (noche), muestra la notificación de noche
+          this.mostrarNotificacionNoche();
+        }
+      });
+    } catch (error) {
       console.error('Error al obtener datos de la base de datos:', error);
-    });
+    }
+  }
+
+  ngOnDestroy() {
+    // Darse de baja de las suscripciones cuando el componente se destruye
+    if (this.luzSubscription) {
+      this.luzSubscription(); // Llama a la función Unsubscribe
+    }
   }
 
   async mostrarNotificacionDia() {
@@ -37,9 +48,7 @@ export class HomePage implements OnInit {
           title: "BUEN DIA, ESTA SOLEADO",
           body: "¡No olvides protegerte del sol!",
           id: 1,
-          schedule:{
-  
-          }
+          schedule:{ }
         }
       ]
     });
@@ -52,9 +61,7 @@ export class HomePage implements OnInit {
           title: "YA ANOCHECIÓ!",
           body: "¡DUEEEERMETEEEEEEEEEEEEEEEEEEEEEEEEEEE!",
           id: 2,
-          schedule:{
-            allowWhileIdle:true
-          }
+          schedule:{ }
         }
       ]
     });
